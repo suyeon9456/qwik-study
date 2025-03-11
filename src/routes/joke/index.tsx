bcc425e9412ca24dd5d9c485eaf5e2b98d58732e
suyeon9456 <suyeon9456@naver.com>
2025-03-11 19:02:46 +0900

    feat: loading data로 임의데이터 불러오기

--- src/routes/joke/index.tsx
+++ src/routes/joke/index.tsx
@@ -1,5 +1,23 @@
 import { component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+
+export const useDadJoke = routeLoader$(async () => {
+  const response = await fetch("https://icanhazdadjoke.com/", {
+    headers: { Accept: "application/json" },
+  });
+  return (await response.json()) as {
+    id: string;
+    status: number;
+    joke: string;
+  };
+});
 
 export default component$(() => {
-  return <section class="section bright">A Joke!</section>;
+  const dadJokeSignal = useDadJoke();
+  return (
+    <section class="section bright">
+      A Joke!!
+      {dadJokeSignal.value.joke}
+    </section>
+  );
 });
